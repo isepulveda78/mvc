bda3738ea4bb83edacf92c8be34d805246cc3d55
Israel Sepulveda <sepulveda.israel@gmail.com>
2018-12-05 12:03:48 -0500

    MVC framework

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  //DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '__YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
+  //App Root
+  define ('APPROOT', dirname(dirname(__FILE__)));
+  //URL Root
+  define ('URLROOT', '_YOUR_URL_');
+  //Site name
+  define ('SITENAME', '_YOUR_SITENAME_');
+ ?>
---
+++ app/views/inc/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/style.css">
+    <title><?php echo SITENAME; ?></title>
+  </head>
+  <body>
